8d63ce27d9ddd96d1c353c8c4a2d1ab95b996956
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-08 12:37:50 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Juan Carlos Robledo Morales on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
