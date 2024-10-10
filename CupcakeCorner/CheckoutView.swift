e9fe468740ed034e00d8f9b3c53a6e8c39262acd
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-10 14:12:06 -0600

    Checking for a valid address

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Juan Carlos Robledo Morales on 10/10/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
